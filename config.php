413ac78ac8bd22003f3544719033acc53a0cf2f4
Izahhoney <2020489338@student.uitm.edu.my>
2022-06-26 15:06:42 +0800

    fdgfhy

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname= "projectairasia";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn -> error){
+  die ("Connection failed: " . $conn -> connection_error);
+} 
+?>
